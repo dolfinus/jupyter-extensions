9b24ded58e3c4dee1b1e062fbe86f17d554d3d16
Krishnan R <krishnanr1997@gmail.com>
2021-06-11 20:25:58 +0200

    Refactor Message Handling

--- SparkConnector/src/widgets/sparkconnector.ts
+++ SparkConnector/src/widgets/sparkconnector.ts
@@ -1,5 +1,5 @@
 import { ReactWidget } from '@jupyterlab/apputils';
-import { JSONObject, JSONValue } from '@lumino/coreutils';
+import { JSONObject } from '@lumino/coreutils';
 import {
   ConnectionStatus,
   IComm,
@@ -77,26 +77,35 @@ class StateHandler {
     return notebookId == this.notebooks.currentWidget.id;
   }
 
-  open(notebookId: string, openMsg: JSONValue): NotebookComm {
-    let notebookComm: NotebookComm;
-    notebookComm = this.states.get(notebookId);
+  open(notebookId: string): NotebookComm {
+    const notebookComm: NotebookComm = this.states.get(notebookId);
     if (notebookComm.comm.isDisposed) {
       console.log('conn disposed ' + notebookId);
       return notebookComm;
     }
-    notebookComm.comm.send(openMsg).done.then(() => {
-      console.log('conn restored ' + notebookId);
-    });
+    /* TODO this is invalid for our current backend
+     notebookComm.comm.send({
+              type: 'action',
+              action: 'sparkconn-action-open',
+            }).done.then(() => {
+       console.log('conn restored ' + notebookId);
+     });
+     */
   }
 
-  create(notebook: NotebookPanel, openMsg: JSONValue): NotebookComm {
+  create(notebook: NotebookPanel): NotebookComm {
     let kernel = notebook.sessionContext.session.kernel;
 
     let comm = kernel.createComm('SparkConnector');
-    comm.open(openMsg).done.then(() => {
-      this.states.set(notebook.id, notebookComm);
-      console.log('conn opened ' + notebook.title.label);
-    });
+    comm
+      .open({
+        type: 'action',
+        action: 'sparkconn-action-open',
+      })
+      .done.then(() => {
+        this.states.set(notebook.id, notebookComm);
+        console.log('conn opened ' + notebook.title.label);
+      });
     comm.onClose = () => {
       this.states.delete(notebook.id);
       console.log('conn closed ' + notebook.title.label);
@@ -168,17 +177,12 @@ export class SparkConnector extends ReactWidget {
         this.updateCurrent(undefinedStates.loading);
 
         nbPanel.sessionContext.ready.then(() => {
-          let notebookComm: NotebookComm;
-          let title = nbPanel.title.label;
-          let kernel = nbPanel.sessionContext.session.kernel;
-
+          const title = nbPanel.title.label;
+          const kernel = nbPanel.sessionContext.session.kernel;
           if (this.statehandler.has(nbPanel.id)) {
             // if we already have a comm, connectionStatusChanged wont be triggered,
             // manually send sparkconn-action-open
-            this.statehandler.open(nbPanel.id, {
-              type: 'action',
-              action: 'sparkconn-action-open',
-            });
+            this.statehandler.open(nbPanel.id);
           } else if (kernel.connectionStatus == 'connected') {
             // if we do not have comm and kernel connected, connectionStatusChanged wont be triggered
             // and e need to reconnect
@@ -189,152 +193,24 @@ export class SparkConnector extends ReactWidget {
           kernel.connectionStatusChanged.connect(
             (conn: IKernelConnection, status: ConnectionStatus) => {
               if (status == 'connected') {
-                console.log('notebook connected ' + title);
-                notebookComm = this.statehandler.create(nbPanel, {
-                  type: 'action',
-                  action: 'sparkconn-action-open',
-                });
+                console.log(
+                  'SparkConnector: Notebook Kernel Connected ',
+                  title
+                );
+                const notebookComm = this.statehandler.create(nbPanel);
                 notebookComm.comm.onMsg = (msg: KernelMessage.ICommMsgMsg) => {
-                  console.log('COMM Message', msg.content.data);
                   if (
                     this.notebooks.currentWidget == null ||
                     this.notebooks.currentWidget.id != nbPanel.id
                   ) {
                     return;
                   }
-
-                  if (msg.content.data.msgtype == 'sparkconn-action-open') {
-                    if (msg.content.data.page == 'sparkconn-config') {
-                      let currentConfig;
-                      if (nbPanel.model.metadata.has('sparkconnect')) {
-                        currentConfig = nbPanel.model.metadata.get(
-                          'sparkconnect'
-                        ) as unknown as SparkconnectMetadata;
-                      } else {
-                        currentConfig = {
-                          bundled_options: [],
-                          list_of_options: [],
-                        } as SparkconnectMetadata;
-                      }
-                      console.log(
-                        title +
-                          ' config-msg=' +
-                          JSON.stringify(msg.content.data) +
-                          ' received'
-                      );
-                      console.log(
-                        title + ' meta=' + JSON.stringify(currentConfig)
-                      );
-                      notebookComm.states.configuring.init(
-                        title,
-                        msg.content.data.maxmemory as string,
-                        msg.content.data.sparkversion as string,
-                        msg.content.data.cluster as string,
-                        currentConfig,
-                        msg.content.data.availableoptions as JSONObject,
-                        msg.content.data.availablebundles as JSONObject
-                      );
-                      notebookComm.states.configuring.onConnect.connect(
-                        (
-                          configuring: ConfigurationState<IState>,
-                          message: JSONObject
-                        ) => {
-                          // Connect button clicked
-                          notebookComm.states.connecting.init(
-                            title,
-                            msg.content.data.sparkversion as string,
-                            msg.content.data.cluster as string
-                          );
-                          this.updateCurrent(notebookComm.states.connecting);
-                          nbPanel.model.metadata.set(
-                            'sparkconnect',
-                            message['metadata']
-                          );
-                          notebookComm.comm.send({
-                            type: 'action',
-                            action: 'sparkconn-action-connect',
-                            'action-data': { options: message['options'] },
-                          });
-                        }
-                      );
-                      this.updateCurrent(notebookComm.states.configuring);
-                    }
-                  } else if (
-                    msg.content.data.msgtype == 'sparkconn-connected'
-                  ) {
-                    const config: any = msg.content.data.config;
-                    notebookComm.states.connected.init(
-                      title,
-                      config.sparkhistoryserver as string
-                    );
-                    //when the sparkconnector goes into connected state,
-                    //kernel restarts are the equivalent of a reload of the entire connection
-                    kernel.statusChanged.connect((_, status) => {
-                      if (status == 'restarting') {
-                        kernel.reconnect().then(() => {
-                          this.statehandler = new StateHandler(this.notebooks);
-                          this.initStateHandling();
-                        });
-                      }
-                    });
-                    notebookComm.states.connected.onReconfigure.connect(() => {
-                      // Connect button clicked
-                      this.updateCurrent(undefinedStates.loading);
-                      notebookComm.comm.send({
-                        type: 'action',
-                        action: 'sparkconn-action-disconnect',
-                      });
-                      notebookComm.notebook.sessionContext.session.kernel.restart();
-                    });
-                    this.updateCurrent(notebookComm.states.connected);
-                  } else if (
-                    msg.content.data.msgtype == 'sparkconn-config' ||
-                    msg.content.data.msgtype == 'sparkconn-disconnected'
-                  ) {
-                    notebookComm.comm.send({
-                      type: 'action',
-                      action: 'sparkconn-action-open',
-                    });
-                  } else if (
-                    msg.content.data.msgtype == 'sparkconn-connect-error'
-                  ) {
-                    console.log(
-                      title +
-                        ' error-msg=' +
-                        JSON.stringify(msg.content.data) +
-                        ' received'
-                    );
-                    notebookComm.states.connectfailed.init(
-                      title,
-                      msg.content.data.error as string
-                    );
-
-                    notebookComm.states.connectfailed.onReconfigure.connect(
-                      () => {
-                        this.updateCurrent(undefinedStates.loading);
-                        notebookComm.comm.send({
-                          action: 'sparkconn-action-disconnect',
-                        });
-
-                        // Restart the kernel, because SparkContexts are cached,
-                        // we need to restart to do a clean retry again
-                        notebookComm.notebook.sessionContext.session.kernel.restart();
-                      }
-                    );
-                    this.updateCurrent(notebookComm.states.connectfailed);
-                  } else if (
-                    msg.content.data.msgtype == 'sparkconn-action-follow-log'
-                  ) {
-                    if (this.currentState.name() == 'connected') {
-                      notebookComm.states.connected.log(
-                        msg.content.data.msg as string
-                      );
-                    } else if (this.currentState.name() == 'connecting') {
-                      notebookComm.states.connecting.log(
-                        msg.content.data.msg as string
-                      );
-                    }
-                  }
+                  this.onCommMessage(
+                    msg,
+                    notebookComm.notebook,
+                    notebookComm,
+                    kernel
+                  );
                 };
               } else if (status == 'connecting') {
                 this.updateCurrent(undefinedStates.loading);
@@ -347,6 +223,157 @@ export class SparkConnector extends ReactWidget {
         });
       }
     );
+  }
+
+  /*
+    Handle messages from the Kernel extension.
+  */
+  onCommMessage(
+    message: KernelMessage.ICommMsgMsg,
+    notebookPanel: any,
+    notebookComm: NotebookComm,
+    kernel: IKernelConnection
+  ) {
+    console.debug(
+      `SparkConnector: Comm Message ${message.content.data.msgtype} for notebook ${notebookPanel}:\n`,
+      message.content.data
+    );
+    const data: any = message.content.data;
+    const title = notebookPanel.title.label; // TODO Remove this
+    let undefinedStates = new UndefinedStates();
+    switch (data.msgtype) {
+      case 'sparkconn-action-open':
+        const page = message.content.data.page;
+        if (page == 'sparkconn-config') {
+          const currentConfig =
+            this.getCurrentConfigFromNotebook(notebookPanel);
+          notebookComm.states.configuring.init(
+            title,
+            data.maxmemory as string,
+            data.sparkversion as string,
+            data.cluster as string,
+            currentConfig,
+            data.availableoptions as JSONObject,
+            data.availablebundles as JSONObject
+          );
+          // Connect button clicked
+          notebookComm.states.configuring.onConnect.connect(
+            (
+              configuring: ConfigurationState<IState>,
+              connectMessage: JSONObject
+            ) => {
+              
+              notebookComm.states.connecting.init(
+                title,
+                data.sparkversion as string,
+                data.cluster as string
+              );
+              this.updateCurrent(notebookComm.states.connecting);
+              this.setCurrentConfigToNotebook(
+                notebookPanel,
+                connectMessage['metadata'] as any
+              );
+              notebookComm.comm.send({
+                type: 'action',
+                action: 'sparkconn-action-connect',
+                'action-data': { options: connectMessage['options'] },
+              });
+            }
+          );
+          this.updateCurrent(notebookComm.states.configuring);
+        } else if (page == 'sparkconn-auth') {
+          // TODO switch to authentication page
+        } else if (page == 'sparkconn-connected') {
+          // The kernel sends this page when a comm is opened, but the
+          // user is already connected. It subsequently also sends a msgtype: sparkconn-connected,
+          // so we don't do anything here
+        }
+        break;
+
+      case 'sparkconn-connected':
+        notebookComm.states.connected.init(
+          title,
+          data.config.sparkhistoryserver as string
+        );
+        //when the sparkconnector goes into connected state,
+        //kernel restarts are the equivalent of a reload of the entire connection
+        kernel.statusChanged.connect((_, status) => {
+          // TODO What is this:
+          if (status == 'restarting') {
+            kernel.reconnect().then(() => {
+              this.statehandler = new StateHandler(this.notebooks);
+              this.initStateHandling();
+            });
+          }
+        });
+        notebookComm.states.connected.onReconfigure.connect(() => {
+          // Connect button clicked
+          this.updateCurrent(undefinedStates.loading);
+          notebookComm.comm.send({
+            type: 'action',
+            action: 'sparkconn-action-disconnect',
+          });
+          notebookComm.notebook.sessionContext.session.kernel.restart();
+        });
+        this.updateCurrent(notebookComm.states.connected);
+        break;
+
+      case 'sparkconn-config':
+        // Sent by kernel on successful authentication
+        // TODO Switch to Config Page
+        break;
+
+      case 'sparkconn-connect-error':
+        notebookComm.states.connectfailed.init(title, data.error as string);
+        notebookComm.states.connectfailed.onReconfigure.connect(() => {
+          this.updateCurrent(undefinedStates.loading);
+          notebookComm.comm.send({
+            action: 'sparkconn-action-disconnect',
+          });
+          // Restart the kernel, because SparkContexts are cached,
+          // we need to restart to do a clean retry again
+          notebookComm.notebook.sessionContext.session.kernel.restart();
+        });
+        this.updateCurrent(notebookComm.states.connectfailed);
+        break;
+
+      case 'sparkconn-action-follow-log':
+        if (this.currentState.name() == 'connected') {
+          notebookComm.states.connected.log(data.msg as string);
+        } else if (this.currentState.name() == 'connecting') {
+          notebookComm.states.connecting.log(data.msg as string);
+        }
+        break;
+
+      default:
+        console.error(
+          'SparkConnector: Received an unknown msgtype from kernel:',
+          message
+        );
+        break;
+    }
+  }
+
+  getCurrentConfigFromNotebook(notebookPanel: NotebookPanel) {
+    let currentConfig;
+    if (notebookPanel.model.metadata.has('sparkconnect')) {
+      currentConfig = notebookPanel.model.metadata.get(
+        'sparkconnect'
+      ) as unknown as SparkconnectMetadata;
+    } else {
+      currentConfig = {
+        bundled_options: [],
+        list_of_options: [],
+      } as SparkconnectMetadata;
+    }
+    return currentConfig;
+  }
+
+  setCurrentConfigToNotebook(
+    notebookPanel: NotebookPanel,
+    config: SparkconnectMetadata
+  ) {
+    notebookPanel.model.metadata.set('sparkconnect', config as any);
   }
 
   render(): JSX.Element {
